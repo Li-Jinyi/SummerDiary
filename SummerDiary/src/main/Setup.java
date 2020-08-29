9efe2f0dce3f788cf56bfe370350fa676a633653
jinyi <jinyi4023@outlook.com>
2020-08-29 21:35:24 +0100

    CardsTHREE completed

--- SummerDiary/src/main/Setup.java
+++ SummerDiary/src/main/Setup.java
@@ -21,7 +21,7 @@ public class Setup
 		cardsONE[1] = new Card(2, 	"真人剧本杀", 				1, 2, new int[] {4, 2, 0}, 	null, "我福尔摩斯不需要别人的帮助！");
 		cardsONE[2] = new Card(3, 	"回一中", 					1, 3, new int[] {8, 6, 10}, null, "哦， 是你啊");
 		cardsONE[3] = new Card(4, 	"KTV小跳蛙！", 				1, 2, new int[] {6, 4, 8}, 	null, "见习魔法师");
-		cardsONE[4] = new Card(5, 	"海底捞", 					1, 2, new int[] {6, 4, 2}, 	null, "有点尬");
+		cardsONE[4] = new Card(5, 	"海底捞", 					1, 2, new int[] {6, 4, 2}, 	null, null);
 		cardsONE[5] = new Card(6, 	"密室逃脱", 					2, 3, new int[] {8, 6, 2}, 	null, "不想和陌生人组队");
 	}
 	
@@ -29,11 +29,11 @@ public class Setup
 	{
 		cardsTWO = new Card[6];
 		
-		cardsTWO[0] = new Card(7, 	"听说罗小黑大电影上映了", 		1, 2, new int[] {6, 4, 4}, 	null, "零食套餐我都买不了");
+		cardsTWO[0] = new Card(7, 	"听说罗小黑大电影上映了", 		1, 2, new int[] {6, 4, 4}, 	null, null);
 		cardsTWO[1] = new Card(8, 	"一起来L4D2吧！", 			1, 1, new int[] {6, 4, 2}, 	null, "你们三个怎么落单了");
 		cardsTWO[2] = new Card(9, 	"MC才是坠吼滴！", 			1, 1, new int[] {8, 6, 2}, 	null, "为什么要开服务器？");
-		cardsTWO[3] = new Card(10, 	"锡惠公园好像也不错", 			2, 2, new int[] {8, 6, 12}, null, "蓝蓝的天空银河里");
-		cardsTWO[4] = new Card(11, 	"啊太湖的水我的泪", 			2, 3, new int[] {10, 8, 6}, null, "有只小白船");
+		cardsTWO[3] = new Card(10, 	"锡惠公园好像也不错", 		2, 2, new int[] {8, 6, 12}, null, null);
+		cardsTWO[4] = new Card(11, 	"啊太湖的水我的泪", 			2, 3, new int[] {10, 8, 6}, null, "蓝蓝的天空银河里");
 		cardsTWO[5] = new Card(12, 	"For The King！！", 			1, 1, new int[] {8, 6}, 	null, "一人分饰多角");
 	}
 	
@@ -41,12 +41,12 @@ public class Setup
 	{
 		cardsTHREE = new Card[6];
 		
-		cardsTHREE[0] = new Card(13, "我抢到CP展的门票了", 		0, 0, new int[] {}, null, "");
-		cardsTHREE[1] = new Card(14, "逛GAI", 					0, 0, new int[] {}, null, "");
-		cardsTHREE[2] = new Card(15, "家庭旅行", 					0, 0, new int[] {}, null, "");
-		cardsTHREE[3] = new Card(16, "巴蜀三日游", 				0, 0, new int[] {}, null, "");
-		cardsTHREE[4] = new Card(17, "游戏，不打不行！", 			0, 0, new int[] {}, null, "");
-		cardsTHREE[5] = new Card(18, "帮TA穿女装", 				0, 0, new int[] {}, null, "");
+		cardsTHREE[0] = new Card(13, "我抢到CP展的门票了", 		2, 2, new int[] {10, 6}, 	null, "房费都没人平摊");
+		cardsTHREE[1] = new Card(14, "逛GAI", 					1, 1, new int[] {8, 6, 2}, 	null, null);
+		cardsTHREE[2] = new Card(15, "家庭旅行", 				2, 2, new int[] {10, 8, 6}, null, "和睦的家庭");
+		cardsTHREE[3] = new Card(16, "巴蜀三日游", 				3, 3, new int[] {10, 8, 6}, null, "医院人好多");
+		cardsTHREE[4] = new Card(17, "游戏，不打不行！", 			1, 1, new int[] {6, 4}, 	null, null);
+		cardsTHREE[5] = new Card(18, "帮TA穿女装", 				1, 3, new int[] {12, 6}, 	null, "斯巴拉西");
 	}
 	
 	public void setupCardsFOUR()
@@ -54,10 +54,10 @@ public class Setup
 		cardsFOUR = new Card[6];
 		
 		cardsFOUR[0] = new Card(19,  "啊啊啊要来不及写作业了！", 	0, 0, new int[] {}, null, "");
-		cardsFOUR[1] = new Card(20,  "作业？我就是要打游戏！", 		0, 0, new int[] {}, null, "");
+		cardsFOUR[1] = new Card(20,  "作业？我就是要打游戏！", 	0, 0, new int[] {}, null, "");
 		cardsFOUR[2] = new Card(21,  "再来一次海底捞吧", 			0, 0, new int[] {}, null, "");
 		cardsFOUR[3] = new Card(22,  "甄嬛传还是好看的呀", 		0, 0, new int[] {}, null, "");
-		cardsFOUR[4] = new Card(23,  "去迪士尼VAN~", 				0, 0, new int[] {}, null, "");
+		cardsFOUR[4] = new Card(23,  "去迪士尼VAN~", 			0, 0, new int[] {}, null, "");
 		cardsFOUR[5] = new Card(24,  "谁是运气王！", 				0, 0, new int[] {}, null, "");
 	}
 	
