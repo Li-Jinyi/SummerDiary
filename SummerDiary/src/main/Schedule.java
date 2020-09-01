d9ae86cedd4194b13f3734edc0d8225d419e430e
jinyi <jinyi4023@outlook.com>
2020-09-01 21:17:43 +0100

    New Methods

--- SummerDiary/src/main/Schedule.java
+++ SummerDiary/src/main/Schedule.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Schedule 
 {
 	private int[] week;
@@ -12,5 +15,56 @@ public class Schedule
 	public void setWeek(int[] week)
 	{
 		this.week = week;
+	}
+	
+	public void setEmpty(int[] week)
+	{
+		for (int i = 0; i < week.length; i++)
+		{
+			week[i] = 0;
+		}
+	}
+	
+	public boolean isEmpty(int[] week, int day)
+	{
+		if (week[day-1] == 0)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void occupy(int[] week, int day)
+	{
+		week[day-1] = 1;
+	}
+	
+	public int[] daysEmpty(int[] week)
+	{
+		int index = 0;
+		
+		for(int i = 0; i < week.length; i++)
+		{
+			if (week[i] == 1)
+			{
+				week[index++] = i + 1;
+			}
+		}
+		
+		int[] result = new int[index];
+		System.arraycopy(week, 0, result, 0, index);
+		
+		return result;
+	}
+	
+	public boolean isWenEmpty(int[] week)
+	{
+		if (week[2] == 0)
+		{
+			return true;
+		}
+		
+		return false;
 	}
 }
