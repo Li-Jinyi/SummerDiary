b7160f67b38c8a700141416390a6fc519dec781c
jinyi <jinyi4023@outlook.com>
2020-09-01 20:59:15 +0100

    New Class Schedule

---
+++ SummerDiary/src/main/Schedule.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Schedule 
+{
+	private int[] week;
+	
+	public int[] getWeek()
+	{
+		return week;
+	}
+	
+	public void setWeek(int[] week)
+	{
+		this.week = week;
+	}
+}
